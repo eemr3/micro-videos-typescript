8f14b016bcdc0f60fd18259b1e3a33c0cfb102cd
Emerson <eemr3@yahoo.com.br>
2023-02-13 16:43:16 -0300

    test: should accept a uuid passed in constructor

--- src/@seedworkd/domain/unique-entity-id.vo.spec.ts
+++ src/@seedworkd/domain/unique-entity-id.vo.spec.ts
@@ -8,4 +8,12 @@ describe('UniqueEntityId unit Test', () => {
     expect(() => new UniqueEntityId('fake id')).toThrow(new InvalidUuidError());
     expect(validateSpy).toHaveBeenCalled();
   });
+
+  it('should accept a uuid passed in constructor', () => {
+    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, 'validate');
+    const vo = new UniqueEntityId('87cba71d-4ed4-411f-ba4b-41fccb328f98');
+
+    expect(vo.id).toBe('87cba71d-4ed4-411f-ba4b-41fccb328f98');
+    expect(validateSpy).toHaveBeenCalled();
+  });
 });
