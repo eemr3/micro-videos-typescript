273e761f929cd5d2fad9bf331804a407e9180837
Emerson <eemr3@yahoo.com.br>
2023-02-13 16:11:23 -0300

    refactor: add value objet UniqueEnttyId in Category

--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,4 +1,4 @@
-import { v4 as uuidv4 } from 'uuid';
+import UniqueEntityId from '../../../@seedworkd/domain/unique-entity-id.vo';
 export type CategoryProperties = {
   name: string;
   description?: string;
@@ -7,10 +7,10 @@ export type CategoryProperties = {
 };
 
 export class Category {
-  public readonly id: string;
+  public readonly id: UniqueEntityId;
 
-  constructor(public readonly props: CategoryProperties, id?: string) {
-    this.id = id || uuidv4();
+  constructor(public readonly props: CategoryProperties, id?: UniqueEntityId) {
+    this.id = id || new UniqueEntityId();
     this.description = this.props.description;
     this.isActive = this.props.isActive;
     this.props.createdAt = this.props.createdAt ?? new Date();
