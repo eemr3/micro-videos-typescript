bae6d791ed75f0482a8ca576db8ae5b2fd97a24d
Emerson <eemr3@yahoo.com.br>
2023-02-13 16:47:51 -0300

    test: should a valid uuid must be created

--- src/@seedworkd/domain/unique-entity-id.vo.spec.ts
+++ src/@seedworkd/domain/unique-entity-id.vo.spec.ts
@@ -1,6 +1,6 @@
 import InvalidUuidError from '../errors/invalid-uuid.error';
 import UniqueEntityId from './unique-entity-id.vo';
-
+import { v4 as uuidv4, validate as uuidValidade } from 'uuid';
 describe('UniqueEntityId unit Test', () => {
   it('should throw error when uuid is invalid', () => {
     const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, 'validate');
@@ -14,6 +14,14 @@ describe('UniqueEntityId unit Test', () => {
     const vo = new UniqueEntityId('87cba71d-4ed4-411f-ba4b-41fccb328f98');
 
     expect(vo.id).toBe('87cba71d-4ed4-411f-ba4b-41fccb328f98');
+    expect(validateSpy).toHaveBeenCalled();
+  });
+
+  it('should a valid uuid must be created', () => {
+    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, 'validate');
+    const vo = new UniqueEntityId();
+
+    expect(uuidValidade(vo.id)).toBeTruthy();
     expect(validateSpy).toHaveBeenCalled();
   });
 });
