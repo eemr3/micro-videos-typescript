6807909107a140b8a9c07d4eaa0f835825c12dec
Emerson <eemr3@yahoo.com.br>
2023-02-13 16:36:09 -0300

    test: should throw error when uuid is invalid

---
+++ src/@seedworkd/domain/unique-entity-id.vo.spec.ts
@@ -0,0 +1,11 @@
+import InvalidUuidError from '../errors/invalid-uuid.error';
+import UniqueEntityId from './unique-entity-id.vo';
+
+describe('UniqueEntityId unit Test', () => {
+  it('should throw error when uuid is invalid', () => {
+    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, 'validate');
+
+    expect(() => new UniqueEntityId('fake id')).toThrow(new InvalidUuidError());
+    expect(validateSpy).toHaveBeenCalled();
+  });
+});
