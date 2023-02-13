f53ffb016895e1efe157e28cb55bbf6172d23b24
Emerson <eemr3@yahoo.com.br>
2023-02-13 12:15:37 -0300

    test: create test for properties constructor Category

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,7 +1,50 @@
 import { Category } from './category';
-
+import { omit } from 'lodash';
 describe('Category Test', () => {
-  it('sould constructor of category', () => {
+  it('sould constructor of  property name', () => {
+    const category = new Category({
+      name: 'Move',
+      description: null,
+      isActive: true,
+    });
+    const props = omit(category.props, 'createdAt');
+
+    expect(props).toStrictEqual({
+      name: 'Move',
+      description: null,
+      isActive: true,
+    });
+  });
+
+  it('sould constructor of  properties name, description and isActive', () => {
+    const category = new Category({
+      name: 'Move',
+      description: 'some description',
+      isActive: false,
+    });
+    const props = omit(category.props, 'createdAt');
+    expect(props).toStrictEqual({
+      name: 'Move',
+      description: 'some description',
+      isActive: false,
+    });
+
+    expect(category.props.createdAt).toBeInstanceOf(Date);
+  });
+  it('sould constructor of  properties name, description and isActive', () => {
+    const category = new Category({
+      name: 'Move',
+      description: 'some description',
+      isActive: false,
+    });
+    const props = omit(category.props, 'createdAt');
+    expect(props).toStrictEqual({
+      name: 'Move',
+      description: 'some description',
+      isActive: false,
+    });
+  });
+  it('sould constructor of  properties name, description, isActive, createdAt', () => {
     const createdAt = new Date();
     const category = new Category({
       name: 'Move',
@@ -14,6 +57,42 @@ describe('Category Test', () => {
       name: 'Move',
       description: 'some description',
       isActive: true,
+      createdAt,
+    });
+  });
+  it('sould constructor of  properties name, description', () => {
+    const category = new Category({
+      name: 'Move',
+      description: 'other description',
+    });
+
+    expect(category.props).toMatchObject({
+      name: 'Move',
+      description: 'other description',
+    });
+  });
+
+  it('sould constructor of  properties name, isActive', () => {
+    const category = new Category({
+      name: 'Move',
+      isActive: true,
+    });
+
+    expect(category.props).toMatchObject({
+      name: 'Move',
+      isActive: true,
+    });
+  });
+
+  it('sould constructor of  properties name, createdAt', () => {
+    const createdAt = new Date();
+    const category = new Category({
+      name: 'Move',
+      createdAt,
+    });
+
+    expect(category.props).toMatchObject({
+      name: 'Move',
       createdAt,
     });
   });
